34dec6f3c074a912d78a6d3afb3602f6c3c97e23
diegodu <diegorjunior1999@hotmail.com>
2020-12-03 11:33:10 -0500

    nuevos cambios

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+interface datosTarjeta{
+  imagenP: string,
+  tituloPagina: string;
+  textoB: string;
+  idTarjeta: number;
+
+}
+
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
@@ -11,5 +19,54 @@ export class IndexComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  tarjetas : Array<datosTarjeta> = [
+    {
+      imagenP: "Null",
+      tituloPagina: "Jull",
+      textoB: "El presente blog tiene como objetivo informar acerca de la eficacia de los tratamientos basados en Mindfulness en los trastornos de ansiedad generalizada en la adultez,Para ello se realizó una revisión bibliográfica de tipo retrospectiva, también se redactó un análisiscrítico de los resultados obtenidos en la revisión de la literatura ",
+      idTarjeta: 12
+    },
+    {
+      imagenP: "Hola mundo",
+      tituloPagina: "Jull",
+      textoB: "El presente blog tiene como objetivo informar acerca de la eficacia de los tratamientos basados en Mindfulness en los trastornos de ansiedad generalizada en la adultez,Para ello se realizó una revisión bibliográfica de tipo retrospectiva, también se redactó un análisiscrítico de los resultados obtenidos en la revisión de la literatura ",
+      idTarjeta: 22
+    },
+    {
+      imagenP: "Hola Christian",
+      tituloPagina: "Jull",
+      textoB: "El presente blog tiene como objetivo informar acerca de la eficacia de los tratamientos basados en Mindfulness en los trastornos de ansiedad generalizada en la adultez,Para ello se realizó una revisión bibliográfica de tipo retrospectiva, también se redactó un análisiscrítico de los resultados obtenidos en la revisión de la literatura ",
+      idTarjeta: 23
+    },
+    {
+      imagenP: "Hola mundo",
+      tituloPagina: "Jull",
+      textoB: "El presente blog tiene como objetivo informar acerca de la eficacia de los tratamientos basados en Mindfulness en los trastornos de ansiedad generalizada en la adultez,Para ello se realizó una revisión bibliográfica de tipo retrospectiva, también se redactó un análisiscrítico de los resultados obtenidos en la revisión de la literatura ",
+      idTarjeta: 22
+    },
+    {
+      imagenP: "Hola Christian",
+      tituloPagina: "Jull",
+      textoB: "El presente blog tiene como objetivo informar acerca de la eficacia de los tratamientos basados en Mindfulness en los trastornos de ansiedad generalizada en la adultez,Para ello se realizó una revisión bibliográfica de tipo retrospectiva, también se redactó un análisiscrítico de los resultados obtenidos en la revisión de la literatura ",
+      idTarjeta: 23
+    },
+    {
+      imagenP: "Hola Christian",
+      tituloPagina: "Jull",
+      textoB: "El presente blog tiene como objetivo informar acerca de la eficacia de los tratamientos basados en Mindfulness en los trastornos de ansiedad generalizada en la adultez,Para ello se realizó una revisión bibliográfica de tipo retrospectiva, también se redactó un análisiscrítico de los resultados obtenidos en la revisión de la literatura ",
+      idTarjeta: 23
+    }
+    ,
+    {
+      imagenP: "Hola Christian",
+      tituloPagina: "Jull",
+      textoB: "El presente blog tiene como objetivo informar acerca de la eficacia de los tratamientos basados en Mindfulness en los trastornos de ansiedad generalizada en la adultez,Para ello se realizó una revisión bibliográfica de tipo retrospectiva, también se redactó un análisiscrítico de los resultados obtenidos en la revisión de la literatura ",
+      idTarjeta: 23
+    }
+
+
+
+  ]
 
 }
--- src/app/pages/page2/page2.component.ts
+++ src/app/pages/page2/page2.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface datosSecciones{
+  titulo: string;
+  imgSec: string ;
+  parrafo: string;
+}
+
 @Component({
   selector: 'app-page2',
   templateUrl: './page2.component.html',
@@ -7,9 +14,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Page2Component implements OnInit {
 
+  subtitulo : string = ''
+  tituloPrincipal: String = '';
+  imagenPrincipal: String = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  secciones : Array<datosSecciones> = [
+    {
+      titulo : "soy un titulo con Un Switch444",
+      imgSec : "../../../assets/img/102291825_590345768252505_2729692350740692992_n.jpg",
+      parrafo : "soy un parafoLorem ipsum dolor sit amet consectetur adipisicing elit. Nam autem tempora, quaerat aut quae et enim perferendis libero maxime cum eaque neque facere architecto sit est saepe consectetur modi temporibus?"
+
+    },
+    {
+      titulo : "soy un titulo con Un segundo Switch",
+      imgSec : "../../../assets/img/102291825_590345768252505_2729692350740692992_n.jpg",
+      parrafo : "soy un parrafo Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam autem tempora, quaerat aut quae et enim perferendis libero maxime cum eaque neque facere architecto sit est saepe consectetur modi temporibus?"
+    },
+    {
+      titulo : "null",
+      imgSec : "../../../assets/img/102291825_590345768252505_2729692350740692992_n.jpg",
+      parrafo : "soy un parrafo Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam autem tempora, quaerat aut quae et enim perferendis libero maxime cum eaque neque facere architecto sit est saepe consectetur modi temporibus?"
+    }
+    ,
+    {
+      titulo : "nulaaaaaaaal",
+      imgSec : "../../../assets/img/102291825_590345768252505_2729692350740692992_n.jpg",
+      parrafo : "null"
+
+
+    }
+    ,
+    {
+      titulo : "null",
+      imgSec : "../../../assets/img/102291825_590345768252505_2729692350740692992_n.jpg",
+      parrafo : "soy un parrafo Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam autem tempora, quaerat aut quae et enim perferendis libero maxime cum eaque neque facere architecto sit est saepe consectetur modi temporibus?"
+    }
+  ]
+
 
 }
